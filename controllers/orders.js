e604d49503a687cee3581788c5b240659b13b8ff
VBragado <vicente.bragado@uc.cl>
2023-05-11 18:36:09 -0400

    updated createOrder function
    Llamada POST url_grupo/ordenes-compra/${id} busca en base de datos, crea orden en caso de ser nueva.

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,14 +1,31 @@
+const { Order } = require('../models/order'); // import the Order model
 // Servicio de recepción de órdenes de compra
 const createOrder = async (req, res) => {
   const { cliente, sku, fechaEntrega, cantidad, urlNotificacion } = req.body;
   const orderId = req.params.id;
   try {
-    
+    // check if the order already exists
+    const existingOrder = await Order.findOne({ where: { id: orderId } });
+    if (existingOrder) {
+      return res.status(400).json({ mensaje: 'OC ya fue recibida' });
+    }
+    // create a new order
+    const order = await Order.create({
+      id: orderId,
+      cliente,
+      sku,
+      fechaEntrega,
+      cantidad,
+      urlNotificacion,
+      estado: 'recibida',
+    });
+    res.status(201).json(order);
   } catch (err) {
     console.error(err);
     res.status(500).json({ mensaje: 'Server error' });
   }
 };
+
 
 // Servicio de recepción de actualización de orden de compra
 const updateOrder = async (req, res) => {
--- models/order.js
+++ models/order.js
@@ -1,5 +1,36 @@
-
-// ESTE MODELO NO SE NECESITA, HACEMOS GET /ordenes/:idOrden
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database').sequelize;
+const sequelize = require('../config/database');
 
+const Order = sequelize.define('Order', {
+  id: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    primaryKey: true,
+  },
+  cliente: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  sku: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  fechaEntrega: {
+    type: DataTypes.BIGINT,
+    allowNull: false,
+  },
+  cantidad: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  urlNotificacion: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  estado: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Order;
